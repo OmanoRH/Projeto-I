b9f06e84ef046703481adbc7770356aff70eb20d
StuartMorais <stuartmorais@hotmail.com>
2024-05-29 13:32:00 -0300

    explicação breve do JS do previewImage

--- JS/script.js
+++ JS/script.js
@@ -1,7 +1,11 @@
+//function para a preview da imagem na pagina de cadastro
 function previewImage(){
 
+    //é uma interface do JavaScript que permite que aplicativos da web leiam dados de arquivos de forma assíncrona. 
     var oFReader = new FileReader();
-        oFReader.readAsDataURL(document.getElementById("fileInput").files[0]);
+
+        //O método readAsDataURL do FileReader é utilizado para ler o conteúdo de um arquivo como uma URL de dados codificados em base64.
+        oFReader.readAsDataURL(document.getElementById("fileInput").files[0]); 
 
         oFReader.onload = function (oFREvent) {
             document.getElementById("imgPreview").src = oFREvent.target.result;
