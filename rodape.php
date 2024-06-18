07e228f9d3b5dfb717d5b2fa9f321aaccf5f1ad6
OmanoRH <renatohgsoares@gmail.com>
2024-06-18 11:38:13 -0300

    Footer novo

--- rodape.php
+++ rodape.php
@@ -1,20 +1,52 @@
 <!-- Footer -->
 <!DOCTYPE html>
-<html lang="en">
+<html lang="pt-br">
 
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Document</title>
   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/@mdi/font@7.4.47/css/materialdesignicons.min.css">
 </head>
 
 <body>
+
   <footer class="bg-body-tertiary text-center text-lg-start mt-5">
+
+    <!-- Area de Contatos -->
+    <div id="area-de-contato">
+      <div class="container">
+        <div class="row">
+
+          <!-- Link do About US -->
+          <div class="col-md-12">
+            <h3 class="main-title"><a id="link" href="aboutUs.php">Quem somos Nós</a></h3>
+          </div>
+
+          <!-- Número para Contato -->
+          <div class="col-md-6 contact-box">
+            <span class="numero">
+              <i class="mdi mdi-phone"></i>
+              <p><span class="contact-title">Número para Contato:</span> (83)988974285</p>
+            </span>
+          </div>
+
+
+          <!-- E-mail para Contato -->
+          <div class="col-md-6 contact-box">
+            <span class="numero">
+              <i class="mdi mdi-email"></i>
+              <p><span class="contact-title">E-mail para Contato:</span> boleirosmtfodas@gmail.com</p>
+            </span>
+          </div>
+
+        </div>
+      </div>
+    </div>
+  </footer>
+
+  <!-- <footer class="bg-body-tertiary text-center text-lg-start mt-5">
     <div class="container p-4">
       <div class="row">
 
-        <!-- Email -->
+         Email 
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
           <h5 class="text-uppercase">E-mail</h5>
           <span class="email">
@@ -25,12 +57,11 @@
           </span>
         </div>
 
-        <!-- Contato -->
+         Contato 
         <div class="col-lg-6 col-md-12 mb-4 mb-md-0">
           <h5 class="text-uppercase">Número para contato</h5>
           <span class="numero">
             <i class="mdi mdi-phone"></i>
-
             <p>
               83 98892-2922
             </p>
@@ -38,7 +69,7 @@
         </div>
       </div>
     </div>
-  </footer>
+  </footer> -->
 </body>
 
 </html>
