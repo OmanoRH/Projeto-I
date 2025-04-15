570ac7bc724acd65bc4006266852afce82f01075
Renato Hilquias Guedes Soares <renatohgsoares@gmail.com>
2025-04-15 19:43:14 -0300

    descrições concertadas

--- vulcao.php
+++ vulcao.php
@@ -43,7 +43,7 @@
 
 
         <!-- Texto de introdução -->
-        <p class="parag mb-3">Bolos com uma surpresa vulcânica em seu centro, levam ingredientes simples, majoritariamente chocolate <br> reomendado para festas de aniversário </p>
+        <p class="parag mb-3">Bolos com uma surpresa vulcânica em seu centro, levam ingredientes simples, majoritariamente chocolate <br> recomendado para festas de aniversário </p>
 
         <!--Começo dos Cards de Bolo de Cenoura -->
         <?php
